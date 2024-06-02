852ff07387940a931b9b52cc2b3c905a1b075264
Phan Ha <phanha612@gmail.com>
2024-06-02 23:16:51 +0700

    chore: Update Xcode project settings and dependencies

--- src/Constants/ScreenNames/index.js
+++ src/Constants/ScreenNames/index.js
@@ -1,21 +1,19 @@
 const ScreenNames = {
-  FIRST: "FIRST",
+  DEMO: 'DEMO',
+  FIRST: 'FIRST',
 
   // SPLASH
-  SPLASH: "SPLASH",
+  SPLASH: 'SPLASH',
   // End Splash
 
   // Authentication
-  ABOUT: "ABOUT",
-  LOGIN: "LOGIN",
-  REGISTER: "REGISTER",
-  AUTH_HOME: "AUTH_HOME",
-  ACTIVE_ACCOUNT: "ACTIVE_ACCOUNT",
-  FORGOT_PASSWORD: "FORGOT_PASSWORD",
+  ABOUT: 'ABOUT',
+  LOGIN: 'LOGIN',
+  REGISTER: 'REGISTER',
+  AUTH_HOME: 'AUTH_HOME',
+  ACTIVE_ACCOUNT: 'ACTIVE_ACCOUNT',
+  FORGOT_PASSWORD: 'FORGOT_PASSWORD',
   // End Authentication
-
-  //
-
 };
 
 export default ScreenNames;
